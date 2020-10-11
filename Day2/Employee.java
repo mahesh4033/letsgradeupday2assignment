3d7a0fd2bbfc2bf26e562758ac9d20ea91b27a70
mahesh4033 <70584148+mahesh4033@users.noreply.github.com>
2020-10-11 15:40:38 +0530

    Add files via upload

---
+++ Day2/Employee.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class Employee {
+
+ public String name;
+public   int age;
+ public String city;
+  public void display(){
+
+    System.out.println("The employee name is "+"  "+name);
+    System.out.println(age);
+    System.out.println(city);
+    }
+}
+
+
---
+++ Day2/Main_anotherone.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Main_anotherone {
+
+    public static void main(String[] args) {
+
+        Employee e1=new Employee();
+        e1.name="Mahesh";
+        e1.age=23;
+        e1.city="Berhampur";
+        e1.display();
+        Employee e2=new Employee();
+        e2.name="Miku";
+        e2.age=23;
+        e2.city="Bhubaneswar";
+        e2.display();
+
+
+
+
+
+    }
+}
